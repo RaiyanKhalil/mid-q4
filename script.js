843280f58263fa1eff6d199a4caf407449d7cb92
RaiyanKhalil <raiyankhalil@gmail.com>
2019-06-19 09:37:46 +0600

    MID

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 /* Add your script here */
+var words = ["education", "technology", "mathematics", "science", "engineering", "arithmetic"]
+
+function hangMan(){
+    for(var i = 0; i < words.length; i++){
+        if 
+    }
+}
